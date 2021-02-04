60a2d14115214f6cf69dcea2de01cc56e0ca7d91
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-02-04 16:37:08 +0700

    Add service provider base example UI

--- app/src/main/java/io/tkey/app/ServiceProviderBaseActivity.kt
+++ app/src/main/java/io/tkey/app/ServiceProviderBaseActivity.kt
@@ -1,11 +1,35 @@
 package io.tkey.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import io.tkey.service_provider_base.BaseServiceProvider
+import io.tkey.types.IServiceProvider
 
 class ServiceProviderBaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service_provider_base)
+        setEventHandlers()
+    }
+
+    private val serviceProvider: IServiceProvider =
+        BaseServiceProvider("f1f02ee186749cfe1ef8f957fc3d7a5b7128f979bacc10ab3b2a811d4f990852")
+
+    private fun setEventHandlers() {
+        val input = findViewById<TextInputEditText>(R.id.input)
+        val encrypt = findViewById<Button>(R.id.encrypt)
+        encrypt.setOnClickListener { onEncrypt(input.text.toString()) }
+    }
+
+    private fun onEncrypt(input: String) {
+        consoleLog(serviceProvider.postboxKey.toString(16))
+    }
+
+    private fun consoleLog(text: String) {
+        val console = findViewById<TextView>(R.id.console_output)
+        console.text = text
     }
 }
